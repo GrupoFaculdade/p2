c592ea38a9a5dd8f35bacb725e1d16e555a8ab96
Hidanebr <klebersant@live.com>
2013-11-25 00:46:10 -0200

    1

--- Notafiscal/src/FachadaNota/Fachada.java
+++ Notafiscal/src/FachadaNota/Fachada.java
@@ -13,6 +13,9 @@ import ClasseBasica.Produto;
 import ClasseDados.InterfaceIntensNota;
 import ClasseDados.InterfaceNotaFiscal;
 import ClasseDados.InterfaceProduto;
+import ClasseNegocios.NegociosItensNota;
+import ClasseNegocios.NegociosProduto;
+import Negocio.NegocioNotaFiscal;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -21,6 +24,71 @@ import java.util.ArrayList;
  * @author NeGo
  */
 public class Fachada implements InterfaceIntensNota,InterfaceNotaFiscal,InterfaceProduto {
+
+    NegocioNotaFiscal nota;
+    NegociosProduto prod;
+    NegociosItensNota itensN;
+    
+    public Fachada(){
+        nota = new NegocioNotaFiscal();
+        prod = new NegociosProduto();
+        itensN = new NegociosItensNota();
+    }
+    
+    @Override
+    public void cadastarIntNota(ItensNota cadastarIntN) throws ClassNotFoundException, SQLException, Exception {
+       itensN.alterarIntNota(cadastarIntN);
+    }
+
+    @Override
+    public void alterarIntNota(ItensNota alterarIntN) throws ClassNotFoundException, SQLException, Exception {
+        itensN.
+    }
+
+    @Override
+    public void removerIntNota(ItensNota removerIntN) throws ClassNotFoundException, SQLException, Exception {
+        
+    }
+
+    @Override
+    public ArrayList<ItensNota> listarIntNota(ItensNota listaIntN) throws ClassNotFoundException, SQLException, Exception {
+        
+    }
+
+    @Override
+    public void listaNotaFiscal(int codNotaFiscal) throws ClassNotFoundException, SQLException, NotaFiscalException {
+        
+    }
+
+    @Override
+    public void RemoverNotaFiscal(int codNotaFiscal) throws ClassNotFoundException, SQLException, NotaFiscalException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void alterNotaFiscal(int NotaFiscal, Notaf notaf) throws ClassNotFoundException, SQLException, NotaFiscalException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void cadastrarProd(Produto prod) throws ClassNotFoundException, SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void alterarProd(int p, Produto prod) throws ClassNotFoundException, SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void removerProd(int prod) throws ClassNotFoundException, SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<Produto> listarProd(String listProd) throws ClassNotFoundException, SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
     
 }
